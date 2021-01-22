7f45d41727ee190a553c728bf047a8ceabff1ff9
Coffee-Ovi <ovijit1hsc@gmail.com>
2021-01-22 14:28:02 +0600

    solve problem 1

--- assignment.js
+++ assignment.js
@@ -1,1 +1,15 @@
 // https://github.com/Coffee-Ovi/Assignment-3
+
+//Problem-1
+
+function kilometerToMeter(km){
+    if(km<0){
+        console.log('Error');               ///for nagative value it show a error message.
+    }
+    else{
+        return (km*1000);
+    }
+}
+
+var meter = kilometerToMeter(20);
+//console.log(meter);   for showing output.
