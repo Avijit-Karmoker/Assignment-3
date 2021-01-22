229ba310a4fe10c8706e3f8523a03130551dce13
Coffee-Ovi <ovijit1hsc@gmail.com>
2021-01-22 17:39:38 +0600

    problem 4 done

--- assignment.js
+++ assignment.js
@@ -71,4 +71,14 @@ console.log(totalCost);
 
 //Problem-4
 
-
+function megaFriend(arr){
+    var word = "";
+    for (var i=0; i<arr.length; i++){
+        if(word.length < arr[i].length){
+            word = arr[i];
+        }
+    }
+    return word;
+}
+var bigWord= megaFriend(["Avijit", "Rezaul", "Deep", "Himel", "Rafiquel", "SharminHarami"]);
+console.log(bigWord);
